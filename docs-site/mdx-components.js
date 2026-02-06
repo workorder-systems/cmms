844e6c159a537518846e45a7eef474cdfd6100d4
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2026-02-06 19:01:30 +0100

    Remove deprecated documentation files and configuration for Nextra

--- docs-site/mdx-components.js
+++
@@ -1,8 +0,0 @@
-// Optional MDX components mapping. You can extend this later with
-// custom components that you want to use across your docs.
-export function useMDXComponents(components) {
-  return {
-    ...components,
-  };
-}
-
